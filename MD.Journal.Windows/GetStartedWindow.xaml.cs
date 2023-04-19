1cca109bda9593a1cb2337e667dec7f693fa1c8c
Mark Lauter <mark@marklauter.com>
2023-04-18 22:23:52 -0400

    finished get started page

--- MD.Journal.Windows/GetStartedWindow.xaml.cs
+++ MD.Journal.Windows/GetStartedWindow.xaml.cs
@@ -3,9 +3,11 @@
 
 using MD.Journal.Windows.ViewModels;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using System;
+using System.Threading.Tasks;
+using Windows.Storage;
 using Windows.Storage.Pickers;
-
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
@@ -24,10 +26,20 @@ namespace MD.Journal.Windows
 
         private async void OpenJournalButtonClickAsync(object sender, RoutedEventArgs e)
         {
+            var folder = await this.PickFolderAsync("Open journal");
+            if (folder is not null)
+            {
+                _ = await this.ViewModel.OpenJournalAsync(folder.Path);
+            }
+        }
+
+        private async Task<StorageFolder?> PickFolderAsync(string commitButtonText)
+        {
             var openFolderDialog = new FolderPicker
             {
                 SuggestedStartLocation = PickerLocationId.DocumentsLibrary,
                 ViewMode = PickerViewMode.List,
+                CommitButtonText = commitButtonText,
             };
 
             openFolderDialog.FileTypeFilter.Add("*");
@@ -35,17 +47,51 @@ namespace MD.Journal.Windows
             var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
             WinRT.Interop.InitializeWithWindow.Initialize(openFolderDialog, hwnd);
 
-            var folder = await openFolderDialog.PickSingleFolderAsync();
+            return await openFolderDialog.PickSingleFolderAsync();
+        }
+
+        private async void CreateJournalButtonClickAsync(object sender, RoutedEventArgs e)
+        {
+            var folder = await this.PickFolderAsync("Select journal location");
             if (folder is not null)
             {
-                _ = await this.ViewModel.OpenJournalAsync(folder.Path);
+                var journalName = await this.ShowCreateJournalDialogAsync("Create new journal");
+                if (!String.IsNullOrEmpty(journalName))
+                {
+                    folder = await folder.CreateFolderAsync(journalName);
+                    _ = await this.ViewModel.OpenJournalAsync(folder.Path);
+                }
             }
         }
 
-        private void CreateJournalButtonClick(object sender, RoutedEventArgs e)
+        private async Task<string> ShowCreateJournalDialogAsync(string title)
         {
-            // todo: add create folder dialog
-            throw new NotImplementedException();
+            var journalNameTextBox = new TextBox
+            {
+                Margin = new Thickness(20, 20, 20, 20),
+                AcceptsReturn = false,
+                TextWrapping = TextWrapping.NoWrap,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                Height = 32,
+                FontSize = 14,
+                PlaceholderText = "Journal name",
+                Style = Application.Current.Resources["DefaultTextBoxStyle"] as Style
+            };
+
+            var dialog = new ContentDialog
+            {
+                XamlRoot = this.Content.XamlRoot,
+                Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style,
+                Title = title,
+                Content = journalNameTextBox, //new NewJournalPage(),
+                PrimaryButtonText = "Create journal",
+                CloseButtonText = "Cancel"
+            };
+
+            return await dialog.ShowAsync() == ContentDialogResult.Primary
+                ? journalNameTextBox.Text
+                : String.Empty;
         }
 
         private void RecentJournalsListViewItemClickAsync(object sender, Microsoft.UI.Xaml.Controls.ItemClickEventArgs e)
