09d7dab85ad2f98118bec37a6d587418fa969c7e
Mark Lauter <mark@marklauter.com>
2023-06-08 21:06:31 -0400

    added some system properties

--- MD.Journal.Templates/DocumentTemplate.cs
+++ MD.Journal.Templates/DocumentTemplate.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 
 namespace MD.Journal.Templates;
@@ -13,7 +14,26 @@ public sealed partial class DocumentTemplate
     [GeneratedRegex(@"(?<key>\{{2}\w*\}{2})", RegexOptions.Singleline)]
     private static partial Regex SplitOnFieldPattern();
 
-    private readonly Dictionary<string, string> properties;
+    private readonly Dictionary<string, string> contentProperties;
+
+    private readonly HashSet<string> systemProperties = new(StringComparer.OrdinalIgnoreCase)
+    {
+        "ShortLocalDate",
+        "LongLocalDate",
+        "ShortLocalTime",
+        "LongLocalTime",
+        "ShortLocalDateTime",
+        "LongLocalDateTime",
+
+        "ShortUtcDate",
+        "LongUtcDate",
+        "ShortUtcTime",
+        "LongUtcime",
+        "ShortUtcDateTime",
+        "LongUtcDateTime",
+
+        "Upn",
+    };
 
     public DocumentTemplate(string content)
     {
@@ -23,7 +43,7 @@ public sealed partial class DocumentTemplate
         }
 
         this.Content = content;
-        this.properties = GetFieldKeys(content)
+        this.contentProperties = GetFieldKeys(content)
             .ToDictionary(key => key);
     }
 
@@ -32,7 +52,7 @@ public sealed partial class DocumentTemplate
     {
         foreach (var kvp in record.Properties)
         {
-            if (this.properties.ContainsKey(kvp.Key))
+            if (this.contentProperties.ContainsKey(kvp.Key))
             {
                 this.Write(kvp.Key, kvp.Value);
             }
@@ -51,7 +71,7 @@ public sealed partial class DocumentTemplate
 
     public string Content { get; }
 
-    public IReadOnlyDictionary<string, string> Properties => this.properties;
+    public IReadOnlyDictionary<string, string> Properties => this.contentProperties;
 
     public void Write(string key, string value)
     {
@@ -65,19 +85,19 @@ public sealed partial class DocumentTemplate
             throw new ArgumentException($"'{nameof(value)}' cannot be null or empty.", nameof(value));
         }
 
-        if (!this.properties.ContainsKey(key))
+        if (!this.contentProperties.ContainsKey(key))
         {
             throw new KeyNotFoundException(key);
         }
 
-        this.properties[key] = value;
+        this.contentProperties[key] = value;
     }
 
     public string Read(string key)
     {
         return String.IsNullOrEmpty(key)
             ? throw new ArgumentException($"'{nameof(key)}' cannot be null or empty.", nameof(key))
-            : !this.properties.TryGetValue(key, out var value)
+            : !this.contentProperties.TryGetValue(key, out var value)
                 ? throw new KeyNotFoundException(key)
                 : value;
     }
@@ -89,6 +109,36 @@ public sealed partial class DocumentTemplate
             && line.EndsWith("}}", StringComparison.OrdinalIgnoreCase);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private bool IsSystemProperty(string key)
+    {
+        return this.systemProperties.Contains(key);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private string ReadSystemProperty(string key)
+    {
+        return key switch
+        {
+            "ShortLocalDate" => DateTime.Now.ToShortDateString(),
+            "LongLocalDate" => DateTime.Now.ToLongDateString(),
+            "ShortLocalTime" => DateTime.Now.ToShortTimeString(),
+            "LongLocalTime" => DateTime.Now.ToLongTimeString(),
+            "ShortLocalDateTime" => DateTime.Now.ToString("g", CultureInfo.CurrentCulture),
+            "LongLocalDateTime" => DateTime.Now.ToString("F", CultureInfo.CurrentCulture),
+            "ShortUtcDate" => DateTime.UtcNow.ToShortDateString(),
+            "LongUtcDate" => DateTime.UtcNow.ToLongDateString(),
+            "ShortUtcTime" => DateTime.UtcNow.ToShortTimeString(),
+            "LongUtcime" => DateTime.UtcNow.ToLongTimeString(),
+            "ShortUtcDateTime" => DateTime.UtcNow.ToString("g", CultureInfo.CurrentCulture),
+            "LongUtcDateTime" => DateTime.UtcNow.ToString("F", CultureInfo.CurrentCulture),
+            "Upn" => OperatingSystem.IsWindows()
+                ? System.Security.Principal.WindowsIdentity.GetCurrent().Name
+                : "unknown",
+            _ => String.Empty,
+        };
+    }
+
     public string Render()
     {
         var lines = SplitOnFieldPattern().Split(this.Content);
@@ -97,7 +147,10 @@ public sealed partial class DocumentTemplate
             var line = lines[i];
             if (IsField(line))
             {
-                lines[i] = this.properties[line[2..^2]];
+                var key = line[2..^2];
+                lines[i] = this.IsSystemProperty(key)
+                    ? this.ReadSystemProperty(key)
+                    : this.contentProperties[key];
             }
         }
 
